375325c32c6d5da45ffc0b7fd29916abf990f79e
Andrii Bandura <bbanduraa18@gmail.com>
2022-11-20 01:38:18 +0200

    create smart contract using the own metamask account to transfer the ETH

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transaction.json';
+
+export const contractABI = abi.abi;
+export const contractAddress = '0x6A1400297420281b1616FC6a8547b4314eCF27AD';
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -4,5 +4,11 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.0",
+  networks: {
+    ropsten: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/DXGUAcN3U_ef3lKTVmNnJ5nfQGqnzmoL',
+      accounts: ['992a001da1cec4baed4ebf26b6f8ab7511d23df9452d277817b7645bdfd6bb06']
+    }
+  }
 };
