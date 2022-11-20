0fe81250b3d54073637b49c04e41d62d1ae9948d
Andrii Bandura <bbanduraa18@gmail.com>
2022-11-21 01:18:19 +0200

    refactor Transactions component
    reply dummyData to real transaction from blockchain

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -51,7 +51,7 @@ const gifUrl = useFetch({ keyword });
 }
 
 const Transactions = () => {
-  const { currentAccount } = useContext(TransactionContext);
+  const { currentAccount, transactions } = useContext(TransactionContext);
 
   return (
     <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
@@ -67,7 +67,7 @@ const Transactions = () => {
         )}
 
         <div className="flex flex-wrap justify-center items-center mt-10">
-          {dummyData.reverse().map((transaction, i) => (
+          {transactions.reverse().map((transaction, i) => (
             <TransactionCard key={i} {...transaction} />
           ))}
         </div>
