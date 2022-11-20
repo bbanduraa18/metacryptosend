4e187105a61751a7f47d963e728916f583837704
Andrii Bandura <bbanduraa18@gmail.com>
2022-11-21 01:00:30 +0200

    create and implement checkIfTransactionsExist
    checkIfTransactionsExist - check that have transactions and if have setItem to local storage

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -45,6 +45,19 @@ export const TransactionProvider = ({ children }) => {
     }
   }
 
+  const checkIfTransactionsExist = async () => {
+    try {
+      const transactionContract = getEthereumContract();
+      const transactionCount = await transactionContract.getTransactionCount();
+
+      window.localStorage.setItem('transactionCount', transactionCount);
+    } catch (error) {
+      console.log(error);
+
+      throw new Error('No ethereum object.');
+    }
+  }
+
   const connectWallet = async () => {
     try {
       if (!ethereum) return alert('Please install metamask');
@@ -97,6 +110,7 @@ export const TransactionProvider = ({ children }) => {
 
   useEffect(() => {
     checkIfWalletIsConnected();
+    checkIfTransactionsExist();
   }, []);
 
   return (
